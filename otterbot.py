7326ec6d920cd0188f5f8eb6b3f5fc4435408be6
Nick Ashworth <jna2188@utulsa.edu>
2023-07-28 08:50:01 -0400

    updated otterbot

--- otterbot.py
+++ otterbot.py
@@ -17,13 +17,45 @@ class OtterBot:
 
     def generateMessage(self):
 
-        messageLength = random.randint(1, 4) # how many commands are within the message
+        commandType = random.randint(0, 1) # choice between movement and camera command
 
-        msg = ""
+        if commandType == 0: # do a movement command
+            
+            msg = "!drive " # beginning of a drive command
 
-        for i in range(messageLength): 
+            messageLength = random.randint(1, 4) # how many commands are within the message
 
-            commandType = random.randint(0, 1) # choice between movement and camera command
+            for i in range(messageLength): 
+                
+                movementType = random.randint(1,4) 
 
-            if commandType == 0: # do a movement command
-                print("do something")
+                if movementType == 1: # Go Forward
+                    moveTime = random.randint(5, 15)
+                    msg = msg + f"Go Forward for {str(moveTime)} seconds.  "
+                
+                elif movementType == 2: # Turn Left
+                    angle = random.randint(0, 180)
+                    direction = random.randint(0, 1)
+                    moveTime = random.randint(5, 15)
+
+                    if direction == 0:
+                        msg = msg + f"Turn Left {str(angle)} degrees and move forward {str(moveTime)} seconds.  "
+                    else:
+                        msg = msg + f"Turn Left {str(angle)} degrees and move in reverse {str(moveTime)} seconds.  "
+
+                elif movementType == 3: # Turn Right
+                    angle = random.randint(0, 180)
+                    direction = random.randint(0, 1)
+                    moveTime = random.randint(5, 15)
+
+                    if direction == 0:
+                        msg = msg + f"Turn Right {str(angle)} degrees and move forward {str(moveTime)} seconds.  "
+                    else:
+                        msg = msg + f"Turn Right {str(angle)} degrees and move in reverse {str(moveTime)} seconds.  "
+
+                else: # Reverse
+                    moveTime = random.randint(5, 15)
+                    msg = msg + f"Reverse and travel for {str(moveTime)} seconds.  "
+
+        else:
+            print("test")
